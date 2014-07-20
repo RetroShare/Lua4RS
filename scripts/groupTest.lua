79efe33e197dba4fb5d8eadb58bd6ac07a0a0c10
sehraf <sehraf@users.noreply.github.com>
2014-07-20 12:36:38 +0200

    added some more comments

--- scripts/groupTest.lua
+++ scripts/groupTest.lua
@@ -1,11 +1,21 @@
 rs.clear();
 
+-- get list of oll groups
 grpList = peers.getGroupInfoList()
+
 for i = 1, #grpList do
+	-- get group info
 	grpInfo = grpList[i]
+	
+	-- print group name
 	rs.print(grpInfo["name"])
+		
+	-- check if there is any peer in the group
 	if #grpInfo["peerIds"] > 0 then
+		-- if yes: get peer ids
 		peerIds = grpInfo["peerIds"]
+		
+		-- print their names
 		for j = 1, #peerIds do
 			id = peerIds[j]
 			rs.print(" - " .. peers.getPeerName(id) .. " (" .. id .. ")")
