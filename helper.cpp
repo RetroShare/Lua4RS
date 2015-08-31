2ed088b03e43facd98c1c695ac9d32c93352600f
sehraf <sehraf@users.noreply.github.com>
2015-08-31 16:20:15 +0200

    fixed and improved HTML stripping

--- helper.cpp
+++ helper.cpp
@@ -1,44 +1,19 @@
 #include "helper.h"
-
-void replaceAll(std::string &str, const std::string &from, const std::string &to)
-{
-    if(from.empty())
-        return;
-    size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-        str.replace(start_pos, from.length(), to);
-        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
-    }
-}
+#include <QString>
+#include <QTextDocument>
 
 std::string &stripHTMLTags(std::string &s)
 {
-    // Remove all special HTML characters
-    bool done = false;
-    while(!done)
-    {
-        // Look for start of tag:
-        size_t leftPos = s.find('<');
-        if(leftPos != std::string::npos)
-        {
-            // See if tag close is in this line:
-            size_t rightPos = s.find('>', leftPos);
-            if(rightPos == std::string::npos)
-            {
-                done = true;
-                s.erase(leftPos);
-            }
-            else
-                s.erase(leftPos, rightPos - leftPos + 1);
-        }
-        else
-            done = true;
-    }
+    // std::string to QString
+    QString string = QString::fromUtf8(s.c_str());
 
-    replaceAll(s, "&lt;", "<");
-    replaceAll(s, "&gt;", ">");
-    replaceAll(s, "&amp;", "&");
-    replaceAll(s, "&nbsp;", " ");
+    // extract plain text from HTML
+    QTextDocument td;
+    td.setHtml(string);
+    string = td.toPlainText();
+
+    // QString to std::string
+    s = string.toStdString();
 
     return s;
 }
--- helper.h
+++ helper.h
@@ -3,8 +3,6 @@
 
 #include <string>
 
-void replaceAll(std::string& str, const std::string& from, const std::string& to);
-
 std::string& stripHTMLTags(std::string& s);
 
 #endif // HELPER
