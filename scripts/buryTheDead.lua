cfd20153079bfa13634969e1972d3f492f098f2b
sehraf <sehraf@privatdemail.net>
2017-11-21 15:23:14 +0100

    updated scripts/buryTheDead.lua

--- scripts/buryTheDead.lua
+++ scripts/buryTheDead.lua
@@ -1,105 +1,121 @@
 -- script to move long time offline peers to a seperated group
 -- !!! this may cause a GUI lag !!!
 
--- enter the group name 
-groupName = "Graveyard"
+-- enter the group name
+local groupName = "Graveyard"
 
--- days offline 
-limit = 14
+-- days offline
+local limit = 30
 
-rs.clear()
+-- rs.clear()
 
 -- remove group for offline peers
 function removeGroup()
-	grpList = peers.getGroupInfoList()
-	for i = 1, #grpList do
-		if grpList[i]["name"] == groupName then
-			rs.print("removing " .. grpList[i]["name"] .. " (" .. grpList[i]["id"] .. ")")
-			peers.removeGroup(grpList[i]["id"])
-		end 
-	end
+        local grpList = peers.getGroupInfoList()
+        for i = 1, #grpList do
+                if grpList[i]["name"] == groupName then
+                        rs.print("removing " .. grpList[i]["name"] .. " (" .. grpList[i]["id"] .. ")")
+                        peers.removeGroup(grpList[i]["id"])
+                end
+        end
 end
 
 -- add group for offline peers
 function addGroup()
-	grpInfo = {}
-	grpInfo["flag"] = 0
-	grpInfo["id"] = "0"
-	grpInfo["name"] = groupName
-	grpInfo["peerIds"] = nil
+        local grpInfo = {}
+        grpInfo["flag"] = 0
+        grpInfo["id"] = "0"
+        grpInfo["name"] = groupName
+        grpInfo["peerIds"] = nil
 
-	peers.addGroup(grpInfo)
+        peers.addGroup(grpInfo)
 end
 
 -- check whether the group exists
 function checkGroup()
-	grpList = peers.getGroupInfoList()
-	for i = 1, #grpList do
-		if grpList[i]["name"] == groupName then
-			return true
-		end 
-	end
+        local grpList = peers.getGroupInfoList()
+        for i = 1, #grpList do
+                if grpList[i]["name"] == groupName then
+                        return true
+                end
+        end
 
-	-- didn't fing anything 
-	return false
+        -- didn't fing anything
+        return false
 end
 
 function contains(a, array)
-	for i = 1, #array do
-		if array[i] == a then
-			return true
-		end
-	end
-	return false
+        for i = 1, #array do
+                if array[i] == a then
+                        return true
+                end
+        end
+        return false
 end
 
 function getGroupId()
-	grpList = peers.getGroupInfoList()
-	for i = 1, #grpList do
-		if grpList[i]["name"] == groupName then
-			return grpList[i]["id"]
-		end 
-	end
+        local grpList = peers.getGroupInfoList()
+        for i = 1, #grpList do
+                if grpList[i]["name"] == groupName then
+                        return grpList[i]["id"]
+                end
+        end
+end
+
+function getDays(details)
+        local now = os.time()
+        local diff = now - details["lastUsed"]
+
+        -- how long in days
+        local days = diff / 60 / 60 / 24
+        return math.ceil(days)
 end
 
 -- function to combine everything
 function run()
-	-- create group if needed
-	if checkGroup() ~= true then addGroup() end
+        -- create group if needed
+        if checkGroup() ~= true then addGroup() end
 
-	grpInfo = peers.getGroupInfo( getGroupId() )
-	
-	-- get infos
-	now = os.time()
-	friends = peers.getFriendList()
+        local grpInfo = peers.getGroupInfo( getGroupId() )
 
-	for i = 1, #friends do
-		details = peers.getPeerDetails(friends[i])	
-		diff = now - details["lastUsed"]
+        -- get infos
+        local friends = peers.getFriendList()
 
-		-- get gpg id for groups
-		id = peers.getGPGId(details["id"])		
+        for i = 1, #friends do
+                local details = peers.getPeerDetails(friends[i])
+                -- get gpg id for groups
+                local id = peers.getGPGId(details["id"])
 
-		-- how long in days
-		days = diff / 60 / 60 / 24
-		days = math.ceil(days)
+                local days = getDays(details)
 
-		-- peer offline for too long?
-		if days >= limit then
-			if contains(id, grpInfo["peerIds"]) then
-				rs.print("burying " .. details["name"] .. " (offline for " .. days .. " days)")
-				peers.assignPeerToGroup(grpInfo["id"], id, true)
-			end
-		else
-			if contains(id, grpInfo["peerIds"]) then
-				rs.print("unearthing " .. details["name"])
-				peers.assignPeerToGroup(grpInfo["id"], id, false)
-			end
-		end
-	end
+                -- peer offline for too long?
+                if days >= limit then
+                        if not contains(id, grpInfo["peerIds"]) then
+                                rs.print("burrying " .. details["name"] .. " (offline for " .. days .. " days)")
+                                peers.assignPeerToGroup(grpInfo["id"], id, true)
+                        end
+                end
+        end
+
+        for i = 1, #friends do
+                local details = peers.getPeerDetails(friends[i])
+                -- get gpg id for groups
+                local id = peers.getGPGId(details["id"])
+
+                local days = getDays(details)
+
+                -- peer offline for too long?
+                if days < limit then
+                        if contains(id, grpInfo["peerIds"]) then
+                                rs.print("removing " .. details["name"])
+                                peers.assignPeerToGroup(grpInfo["id"], id, false)
+                        end
+                end
+        end
 end
 
 
 -- use removeGroup() to cleanup your groups (will remove the group for offline peers)
 -- go!
 run()
+rs.print('buryTheDead done')
